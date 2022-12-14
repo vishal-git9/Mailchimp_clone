34f5a1779fbf00b9a3d2186c75b606774f1c5438
vishal-git9 <vishalsinghrajawat990@gmail.com>
2022-12-14 22:39:12 +0530

    day1 is done

---
+++ mailchimp/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from "./Components/navbar"
+import CallToActionWithIllustration from "./Features/features"
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <CallToActionWithIllustration/>
+    </div>
+  );
+}
+
+export default App;
---
+++ mailchimp/src/Components/sidebar.jsx
@@ -0,0 +1,33 @@
+
+import { Box,Text } from "@chakra-ui/react"
+export default function ProductSidebar({isHovering}) {
+  return (
+    <>
+      <Box backgroundColor={"red"}>
+        <Box>
+          <Text>
+            we will
+          </Text>
+          <Text>
+            we will
+          </Text>
+          <Text>
+            we will
+          </Text>
+        </Box>
+        <Box>
+
+        </Box>
+        <Box>
+
+        </Box>
+        <Box>
+
+        </Box>
+        <Box>
+
+        </Box>
+      </Box>
+    </>
+  )
+}
