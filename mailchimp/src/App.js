c117cf38afad35341fc5bc5ef100d211d436a2cd
vishal-git9 <vishalsinghrajawat990@gmail.com>
2022-12-20 01:55:44 +0530

    recent changes are done

--- mailchimp/src/App.js
+++ mailchimp/src/App.js
@@ -1,14 +1,22 @@
-import './App.css';
-import Navbar from "./Components/navbar"
-import Footer from './Components/footer';
-import AllRoutes from './Routes/Allroutes';
-import CallToActionWithIllustration from "./Features/features"
+import "./App.css";
+import Navbar from "./Components/navbar";
+import Footer from "./Components/footer";
+import AllRoutes from "./Routes/Allroutes";
+import CallToActionWithIllustration from "./Features/features";
+import { useLocation } from "react-router-dom";
 function App() {
-  return (
-    <div style={{display:"flex",flexDirection:"column",gap:"120px"}}>
-      <Navbar/>
-      <AllRoutes/>
-      <Footer/>
+  const location = useLocation();
+  let locs = location.pathname;
+  return locs === "/dashboard" ? (
+    <div style={{ display: "flex", flexDirection: "column", gap: "120px" }}>
+      <Navbar />
+      <AllRoutes />
+    </div>
+  ) : (
+    <div style={{ display: "flex", flexDirection: "column", gap: "120px" }}>
+      <Navbar />
+      <AllRoutes />
+      <Footer />
     </div>
   );
 }
